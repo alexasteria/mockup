331a4b906249b2043d001dc15d1daa228fac7d50
alexasteria <replay.faq@gmail.com>
2022-07-12 18:43:14 +0300

    mockup

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useCallback, useState} from 'react';
+import 'antd/dist/antd.css';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import {Avatar, Button, Dropdown, Layout, Menu, PageHeader} from "antd";
+import Page from "./components/page";
+import "./global.css"
+import {MoreOutlined} from "@ant-design/icons";
+const { Content, Footer, Sider } = Layout;
+
+function getItem(
+    label: React.ReactNode,
+    key: React.Key,
+    path: string,
+    icon?: React.ReactNode,
+    children?: MenuItem[]
+): MenuItem {
+    return {
+        key,
+        icon,
+        children,
+        label,
+        path,
+    } as MenuItem;
+}
+
+type MenuItem = {
+    label: string;
+    key: string;
+    path: string;
+    icon: JSX.Element;
+    children?: MenuItem[];
+};
+
+const topMenu: MenuItem[] = [
+    getItem("Мета", "meta", "/meta", undefined,),
+    getItem("Контент", "content", "/content", undefined),
+    getItem("Темы", "themes", "/themes", undefined, ),
+    getItem("Шаблоны", "templates", "/templates", undefined),
+];
+const DropdownMenu = () => (
+    <Dropdown key="more" overlay={<div>Menu</div>} placement="bottomRight">
+        <Button type="text" icon={<MoreOutlined style={{ fontSize: 20 }} />} />
+    </Dropdown>
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+          <div style={{display: "flex", width: '100%'}}>
+              <div className="logo" style={{width: 250, padding: 10}}>Reserved for LOGO</div>
+              <Menu style={{width: "100%"}} onClick={()=>null} selectedKeys={["content"]} mode="horizontal" items={topMenu} />
+              <div style={{display: "flex", padding: 10}}><Avatar src="https://joeschmoe.io/api/v1/random" />
+                  <Button key="3">Выход</Button>
+                  <DropdownMenu key="more" /></div>
+          </div>
+          <Routes>
+              <Route path="/" element={<Page />} />
+              <Route path="/lobnya" element={<Page />} />
+              {/*<Route path="/products" element={<ProductsPage />} />*/}
+              {/*<Route path="/dashboard" element={<DashboardPage />} />*/}
+              {/*<Route path="/orders" element={<OrdersPage />} />*/}
+              {/*<Route path="/setting" element={<SettingPage />} />*/}
+          </Routes>
+                  <Footer style={{ textAlign: "center" }}>
+                      Design Mockup ©2022 Created by @alexasteria
+                  </Footer>
+      </BrowserRouter>
   );
 }
 
