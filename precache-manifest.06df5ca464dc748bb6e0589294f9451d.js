dbf97f230204efa20fa8352e5baaf38668704176
alexasteria <replay.faq@gmail.com>
2022-07-12 19:01:38 +0300

    Updates

---
+++ precache-manifest.06df5ca464dc748bb6e0589294f9451d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c7bed44c5bfb2a67ea9d",
+    "url": "/static/css/main.487b677e.chunk.css"
+  },
+  {
+    "revision": "c7bed44c5bfb2a67ea9d",
+    "url": "/static/js/main.c7bed44c.chunk.js"
+  },
+  {
+    "revision": "3b4bea8583ab514405ce",
+    "url": "/static/css/1.05a5ce14.chunk.css"
+  },
+  {
+    "revision": "3b4bea8583ab514405ce",
+    "url": "/static/js/1.3b4bea85.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "92c36818c434d9c475cdd83bf6ee5576",
+    "url": "/index.html"
+  }
+];
